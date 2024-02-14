ce69c855df3b59e987a56a13dcc661d1c0edf743
Chandana A N <chandanaroyal29@gmail.com>
2024-02-14 01:30:53 -0500

    change paths

--- src/script.js
+++ src/script.js
@@ -1,20 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import ProCards from "./components/ProCards";
 import Header from "./components/Header";
-import { BrowserRouter as Router } from 'react-router-dom';
 import Cart from "./components/Cart";
 
 const AppLayout = () => {
     return (
-        <Router>
-            <div>
-                <Header />
-                <ProCards />
-            </div>
-        </Router>
-
-    )
+        <div className="app">
+            <Header />
+            <Outlet />
+        </div>
+    );
 }
-const root = ReactDOM.createRoot(document.getElementById('container'));
-root.render(<AppLayout />);
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <ProCards />,
+            },
+            {
+                path: "/cart",
+                element: <Cart />,
+            },
+        ],
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("container"));
+root.render(<RouterProvider router={appRouter} />);
